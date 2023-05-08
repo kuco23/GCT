2ad40926eecee55292c42e90d9a0b4ab66d0fca0
Nejc Ševerkar <nseverkar@gmail.com>
2023-05-08 22:26:36 +0200

    change gpt system prompt, logger format

--- lib/logger.py
+++ lib/logger.py
@@ -2,10 +2,11 @@ import logging
 
 def getFileLogger(name):
     # create logger with 'spam_application'
-    logger = logging.getLogger('spam_application')
+    logger = logging.getLogger('trade_logger')
     logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
-    fh = logging.FileHandler('spam.log')
+    fh = logging.FileHandler('trade.log')
     fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(fh)
     return logger
--- trade.py
+++ trade.py
@@ -12,11 +12,11 @@ EVENT_FETCH_INTERVAL = 60 * 30 # 30 minutes
 
 openai_assistant_config = """
 You are an experienced crypto trader. I will provide you with a list of recent crypto articles and
-you will decide which cryptocurrencies to buy or sell. Your response is always in one of two formats:
-"buy <asset> <time>" or "sell <asset>", where "asset" is the symbol of the relevant cryptocurrency,
+you will decide which cryptocurrencies to buy or sell. Your response is always in the form of either
+"buy <asset> <time>" or "sell <asset>", where "asset" is the ticker of the relevant cryptocurrency,
 and "time" is the number of hours for which to hold the bought cryptocurrency. If you want to sell
-all cryptocurrencies respond with "sell all". Please make your responses follow the described format.
-I will then execute the trade for you.
+all cryptocurrencies respond with "sell all". I will then execute the trade for you, though note
+that each trade has fees, so you should only respond when you are confident of the price change.
 """
 exchange_config = {
     'apiKey': config['EXCHANGE_API_KEY'],
